e662cfbbb80ea76e4e6b1031ceb35593c7733b14
長島光太 <se.kokuto@gmail.com>
2018-12-25 00:59:14 +0900

    first

---
+++ Assets/cameraM.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using CnControls;
+
+public class cameraM : MonoBehaviour {
+
+    private float hori = 0;
+
+    void Start ()
+    {
+		
+	}
+	
+	
+	void Update ()
+    {
+        //hori = Input.GetAxis("Horizontal");
+        hori = CnInputManager.GetAxis("Horizontal");
+        this.GetComponent<Transform>().Rotate(0, hori * 0.1f, 0);
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            SceneManager.LoadScene("seni");
+        }
+	}
+}
---
+++ Assets/scenM.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scenM : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SceneSeni()
+    {
+        SceneManager.LoadScene("seni");
+    }
+
+    public void SceneMain()
+    {
+        SceneManager.LoadScene("main");
+    }
+}
